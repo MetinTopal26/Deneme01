2d674fcdb8c9bd7cb8713dc2fac05e3800749eb5
Your Name <you@example.com>
2023-04-15 12:28:33 +0200

    2. kez degisiklik yaptim

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Metin Topal");
         System.out.println("Halloo");
+        System.out.println("Nasilsin?");
     }
 }
